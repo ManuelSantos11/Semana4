8099e3ec6e6c2482990614cad256d2b6d01d1084
ManuelSantos11 <cuatote11@gmail.com>
2017-10-16 16:50:06 -0500

    Ya

--- Hola/src/Main.java
+++ Hola/src/Main.java
@@ -28,6 +28,48 @@ public class Main {
 		double x2= ((-b)- Math.sqrt(Math.pow(b, 2) - (4*a*c)))/(2*a);
 		
 		System.out.println("X1 : "+x1 + "X2 : "+x2);
+		
+		 
+		System.out.println("///////////////////////////////////////////");
+		
+		String [] meses = {"Juan", "Miguel", "Manuel", "Edgar", "Magali", 
+				"Hector", "Alex", "Pablo", "Mago", "Cesar"};
+		
+		String [] sem = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+		
+		System.out.print(meses[0]+" "+meses[6]+" "+meses[9]+" "+meses[4]+" "+sem[2]+" "+sem[6]);
+		
+		boolean x = true;
+		boolean az = false;
+		System.out.println("\n\n");
+		//System.out.println(x != az);
+		
+		if(x == az)
+		{
+			System.out.println("Chanaaaa");
+		}
+		else if(x==az)
+		{
+		System.out.println("Juaanaaa!!");
+		}
+		else if(x==az)
+		{
+			System.out.println("Melon!!!");
+		}
+		else if(!x == !az)
+		{
+			System.out.println("Sandia!!");
+		}
+		else if(!(x == az))
+		{
+			System.out.println("La vieja del otro dia!!");
+		}
+		else
+		{
+			System.out.println("Ni modo!!");
+		}
 	}
 	
-}
+	}
+
+		
